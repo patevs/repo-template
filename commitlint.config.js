01c31c43f08b80eb4e8726df0c3bb223b5686ab2
Patrick Evans <ppevans11@gmail.com>
2020-08-06 16:42:38 +1200

    :wrench: Added commitlint.config.js

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+/* eslint-env node */
+
+module.exports = {
+  extends: ['./node_modules/commitlint-config-gitmoji'],
+  // extends: ['@commitlint/config-conventional'],
+
+  // Add your own rules. See http://marionebl.github.io/commitlint
+  rules: { }
+};
